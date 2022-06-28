238d35df46bb5782c1461ecdad58feb76eb0d2a0
PlanetNamekTech <sr23513@gmail.com>
2022-06-28 14:50:44 -0700

    added some styles and responsiveness

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,14 +1,20 @@
+import './searchBox.css';
+
 const SearchBox = ({search, searchWeather, setSearchQuery}) => {
     return (
-        <div>
-            <label htmlFor="location-input">Input your desired location</label>
+        <div className="search-box">
+            <label className="search-label"
+            htmlFor="location-input">Input your desired location</label>
+            <br />
             <input 
+            className='search-input'
             type="text"
             id="location-input"
             placeholder="London" 
             value={search}
             onChange={(e) => setSearchQuery(e.target.value)} />
             <button
+            className="search-button"
             onClick={() => searchWeather(search)}
             >Search</button>
         </div>
--- src/Components/WeatherCard/WeatherCard.jsx
+++ src/Components/WeatherCard/WeatherCard.jsx
@@ -2,10 +2,12 @@ import './weatherCard.css'
 
 const WeatherCard = ({img, location, temp, condition}) => {
     return ( 
-        <div className="weather-card">
-            <h2>{location}</h2>
-            <img src={img} alt="" />
-            <p className="weather-info">{`It is currently ${condition} and ${temp}\u00B0F in ${location}`}</p>
+        <div className="container">
+            <div className="weather-card">
+                <h2 className="weather-city">{location}</h2>
+                <img className="weather-icon" src={img} alt="" />
+                <p className="weather-info">{`It is currently ${condition} and ${temp}\u00B0F in ${location}`}</p>
+            </div>
         </div>
      );
 }
